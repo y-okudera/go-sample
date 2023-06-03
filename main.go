77aa29e39902270d01745915d0148b486493a086
yuoku <appledev.yuoku@gmail.com>
2023-06-03 23:56:40 +0900

    update main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("start server...")
+	r := gin.Default()
+	r.GET("/hello", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	log.Fatal(r.Run())
+}
